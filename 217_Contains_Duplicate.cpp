4a0e1341cdb2abd2726eb9e2a7f0f46ad0efe863
Mason Wray <macwray17@gmail.com>
2022-12-28 16:26:12 -0700

    217 Contains duplicate

---
+++ 217_Contains_Duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for(int i = 0; i < nums.size(); i++){
+            if (set.count(nums[i]) != 0){
+                return true;
+            }else{
+                set.insert(nums[i]);
+            }
+        }
+        return false;
+    }
+};
