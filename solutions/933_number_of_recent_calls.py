78c4481c8ea8fc912c68bd303b6b9dbc306be28c
bobobubs <macwray17@gmail.com>
2024-05-13 21:41:15 -0500

    933 solution using deque()

--- solutions/933_number_of_recent_calls.py
+++ solutions/933_number_of_recent_calls.py
@@ -1,25 +1,17 @@
+from collections import deque
 class RecentCounter:
 
     def __init__(self):
-        self.requests = []
+        self.requests = deque()
 
     def ping(self, t: int) -> int:
         self.requests.append(t)
         
-        #base case
-        if len(self.requests) <= 1:
-            return 1
-            
-        num_requests = 0
-        for i in self.requests[::-1]:
-            if t - i <= 3000: 
-                num_requests += 1
-        return num_requests
-
-
+        #start from the oldest request and pop if it has been too long
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
+        return len(self.requests)
         
-        
-
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
